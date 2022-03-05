be08f19c7a0384cd965092de3b4b60d3028e98b6
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-03-05 13:08:33 +0530

    v1 dne

--- src/app/Modules/home/profile/profile.component.ts
+++ src/app/Modules/home/profile/profile.component.ts
@@ -89,7 +89,16 @@ export class ProfileComponent implements OnInit {
 
 
  
-
+   findAge(dateString:Date) {
+  var today = new Date();
+  var birthDate = new Date(dateString);
+  var age = today.getFullYear() - birthDate.getFullYear();
+  var m = today.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+  }
+  return age;
+}
   
      
 
