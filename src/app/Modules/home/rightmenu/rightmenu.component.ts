d66362a8ebe2a6e7b07b5535d0a34332307ef597
ArvindKrPrajapati <arvindsoft37@gmail.com>
2022-03-09 23:34:27 +0530

    darkmode

--- src/app/Modules/home/rightmenu/rightmenu.component.ts
+++ src/app/Modules/home/rightmenu/rightmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { ApiService } from 'src/app/api.service';
 
 @Component({
@@ -10,11 +11,21 @@ export class RightmenuComponent implements OnInit {
 
   userdata:any;
   imageurl:string="";
-  constructor(private _api:ApiService) { 
+  constructor(private _api:ApiService,private _meta:Meta) { 
    this.userdata= _api.getUserInfo();  
    this.imageurl=_api.imageurl; 
   }
   ngOnInit(): void {
   }
-
+  changeMode(e:any){
+    var tag;
+    if(e.checked){
+      tag={name:'color-scheme',content:'dark'}
+      this._meta.addTag(tag)
+    }else{
+      tag={name:'color-scheme',content:'light'}
+      this._meta.addTag(tag)
+    }
+   
+  }
 }
